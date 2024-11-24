170326b129d6d323135108ed2248ce0f0b263279
kushal-sinha <kushal147258369@gmail.com>
2024-11-25 02:15:37 +0530

    handling the input from the form

--- NODE-JS-01/app.js
+++ NODE-JS-01/app.js
@@ -1,7 +1,32 @@
 const http = require('http');
+const fs = require('fs');
 
 
 const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>Enter message</title></head>')
+        res.write('<body><form action = "/message" method = "POST"><input type = "text" name = "message"><button type = "submit">Send</button></form></body>')
+        res.write('</html>')
+        return res.end();
+    }
+    if (url === '/message' && method === "POST") {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedbody = Buffer.concat(body).toString();
+            const message = parsedbody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
+        res.statusCode = 302;
+        res.setHeader('location', '/');
+        return res.end();
+
+    }
     console.log(req.url, req.method, req.headers);
     console.log("hello from server");
     res.setHeader('Content-Type', 'text/html');
