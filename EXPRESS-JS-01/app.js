bc16feb646a57c4ad9f105ae1ad6c73702d70bf0
kushal-sinha <kushal147258369@gmail.com>
2024-11-28 00:08:52 +0530

    added styling and navigation

--- EXPRESS-JS-01/app.js
+++ EXPRESS-JS-01/app.js
@@ -1,28 +1,21 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const path = require('path');
+
 
 const app = express();
 app.use(bodyparser.urlencoded({ extended: false }));
 
-//another middleware
-app.use('/add-product', (req, res, next) => {
-    console.log("another Middleware");
-    // res.send('<h1>Add different Products!</h1>');
-    res.send('<form action = "/product" method = "POST"><input type = "text" name = "title"><button type = "submit">Add Product</button></form>')
+const adminRouter = require('./routes/admin');
+const shoprouter = require('./routes/shop');
 
-});
-app.post('/product', (req, res, next) => {
-    console.log(req.body);  // Now it will print the form data
-    console.log("Hello");  // Now it will print the form data
-    res.redirect('/');
-});
+app.use('/admin', adminRouter);
+app.use(shoprouter);
+app.use(express.static(path.join(__dirname, 'public')));
+// sending a 404  not found
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
 
-
-app.use('/', (req, res, next) => {
-    console.log("another Middleware");
-    res.send('<h1>Hello from EXpress!</h1>');
-
-    next();
-});
 
 app.listen(3000);
---
+++ EXPRESS-JS-01/routes/admin.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const rootDir = require('../util/path')
+// importing the router from express works same as app.use , get , post 
+const router = express.Router();
+
+router.get('/add-product', (req, res, next) => {
+    console.log("another Middleware");
+    // res.send('<h1>Add different Products!</h1>');
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+
+});
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body);  // Now it will print the form data
+    console.log("Hello");  // Now it will print the form data
+    res.redirect('/');
+});
+
+module.exports = router;
