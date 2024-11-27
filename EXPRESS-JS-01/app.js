c3a3627d1ccd4f0aaa5c1e276b18b64a32407324
kushal-sinha <kushal147258369@gmail.com>
2024-11-27 12:20:00 +0530

    handling request specifically

--- EXPRESS-JS-01/app.js
+++ EXPRESS-JS-01/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 
 const app = express();
-
+app.use(bodyparser.urlencoded({ extended: false }));
 
 //another middleware
-app.use('/products', (req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log("another Middleware");
-    res.send('<h1>Add different Products!</h1>');
+    // res.send('<h1>Add different Products!</h1>');
+    res.send('<form action = "/product" method = "POST"><input type = "text" name = "title"><button type = "submit">Add Product</button></form>')
+
 });
+app.post('/product', (req, res, next) => {
+    console.log(req.body);  // Now it will print the form data
+    console.log("Hello");  // Now it will print the form data
+    res.redirect('/');
+});
+
+
 app.use('/', (req, res, next) => {
     console.log("another Middleware");
     res.send('<h1>Hello from EXpress!</h1>');
